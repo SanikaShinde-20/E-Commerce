1ec0af1a9a09b6a0d3752a4f7046174643ecbf1d
SanikaShinde-20 <shindesanika765@gmail.com>
2026-01-21 11:36:19 +0530

    add changes made in html css js

--- script.js
+++ script.js
@@ -16,5 +16,62 @@ document.addEventListener('DOMContentLoaded', () => {
     const totalPriceDisplay = document.getElementById('total-price');
     const checkoutBtn = document.getElementById('checkout-btn');
 
+    products.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product');
+        productDiv.innerHTML = `
+        <span>${product.name} - $${product.price.toFixed(2)}</span>
+        <button data-id="${product.id}">Add to Cart</button>
+        `;
+        productList.appendChild(productDiv);
 
+        // 
+    });
+
+    productList.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON') {
+            const productId = parseInt(e.target.getAttribute('data-id'))
+            const product = products.find(p => p.id === productId)
+            addToCart(product)
+        }
+    })
+
+
+    function addToCart(product) {
+        cart.push(product)
+        renderCart()
+    }
+
+
+
+    function renderCart() {
+        cartItems.innerHTML = ''
+        let totalPrice = 0
+
+        if (cart.length) {
+            emptyCartMessage.classList.add('hidden')
+            cartTotalMessage.classList.remove('hidden')
+            cart.forEach((item, index) => {
+                totalPrice += item.price
+                document.createElement('div');
+                const cartItem = document.createElement('div')
+                cartItem.innerHTML = `
+                ${item.name} - $${item.price.toFixed(2)}
+                `
+                cartItems.appendChild(cartItem)
+                totalPriceDisplay.textContent = totalPrice.toFixed(2)
+            })
+
+        } else {
+            emptyCartMessage.classList.add('hidden')
+        }
+    }
+
+    checkoutBtn.addEventListener('click', () => {
+        cart.length = 0
+        alert('Thank you for your purchase!')
+        totalPriceDisplay.textContent = (0).toFixed(2)
+
+        renderCart()
+    })
 })
